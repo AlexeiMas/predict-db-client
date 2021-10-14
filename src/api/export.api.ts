7a3111c3981f6048005a5fe9615c8659a373057e
AlekseyG <agolovin@mentalstack.com>
2021-10-14 22:13:17 +0300

    fix  uriSearchParams

--- src/api/export.api.ts
+++ src/api/export.api.ts
@@ -3,107 +3,51 @@ import { FilterModel } from "shared/models/filters.model";
 import api from "./api.client";
 
 const getApiQuery = (filters?: FilterModel): string => {
-  let searchQuery = "?";
+  const urlSearchParams = new URLSearchParams();
 
   if (filters) {
-    if (filters.modelType) {
-      filters.modelType.forEach((value) => {
-        if (searchQuery !== "?") searchQuery += "&";
-        searchQuery += `modelId=${value}`;
-      });
-    }
+    if (filters.modelType) filters.modelType.forEach((value) => urlSearchParams.append('modelId', `${value}`));
 
     if (filters.geneType) {
-      filters.geneType.genes.forEach((value) => {
-        if (searchQuery !== "?") searchQuery += "&";
-        searchQuery += `gene=${value}`;
-      });
-      filters.geneType.aliases.forEach((value) => {
-        if (searchQuery !== "?") searchQuery += "&";
-        searchQuery += `alias=${value}`;
-      });
-      filters.geneType.proteins.forEach((value) => {
-        if (searchQuery !== "?") searchQuery += "&";
-        searchQuery += `protein=${value}`;
-      });
-
-      if (filters.geneType.includeExpressions !== null) {
-        if (searchQuery !== "?") searchQuery += "&";
-        searchQuery += `includeExpressions=${filters.geneType.includeExpressions}`;
-      }
+      filters.geneType.genes.forEach((value) => urlSearchParams.append('gene', `${value}`));
+      filters.geneType.aliases.forEach((value) => urlSearchParams.append('alias', `${value}`));
+      filters.geneType.proteins.forEach((value) => urlSearchParams.append('protein', `${value}`));
+      if (filters.geneType.includeExpressions !== null) urlSearchParams.append('includeExpressions', `${filters.geneType.includeExpressions}`)
     }
 
     if (filters.tumourType) {
       filters.tumourType.forEach((item) => {
-        if (item.primary) {
-          if (searchQuery !== "?") {
-            item.primary.map(p => searchQuery += `&tumourType=${p}`);
-          } else {
-            for (let i = 0; i < item.length; i++) {
-              if (i === 0) {
-                searchQuery += `tumourType=${item.primary[i]}`;
-              } else {
-                searchQuery += `&tumourType=${item.primary[i]}`;
-              }
-            }
-          }
-        }
-        if (item.sub) {
-          item.sub.forEach((value) => {
-            searchQuery += `&tumourSubType=${value}`;
-          });
-        }
+        if (item.primary) item.primary.forEach(tumourType => urlSearchParams.append('tumourType', tumourType))
+        if (item.sub) item.sub.forEach((value) => urlSearchParams.append('tumourSubType', `${value}`));
       });
     }
 
     if (filters.historyType) {
       filters.historyType.forEach((item) => {
-        if (item.treatment) {
-          item.treatment.forEach((value) => {
-            if (searchQuery !== "?") searchQuery += "&";
-            searchQuery += `historyTreatment=${value}`;
-          });
-        }
-        if (item.response) {
-          item.response.forEach((value) => {
-            if (searchQuery !== "?") searchQuery += "&";
-            searchQuery += `historyResponseType=${value}`;
-          });
-        }
+        if (item.treatment) item.treatment.forEach((value) => urlSearchParams.append('historyTreatment', `${value}`));
+        if (item.response) item.response.forEach((value) => urlSearchParams.append('historyResponseType', `${value}`));
       });
     }
 
     if (filters.responsesType) {
       filters.responsesType.forEach((item) => {
-        if (item.treatment) {
-          item.treatment.forEach((value) => {
-            if (searchQuery !== "?") searchQuery += "&";
-            searchQuery += `responsesTreatment=${value}`;
-          });
-        }
-        if (item.response) {
-          item.response.forEach((value) => {
-            if (searchQuery !== "?") searchQuery += "&";
-            searchQuery += `responsesResponseType=${value}`;
-          });
-        }
+        if (item.treatment) item.treatment.forEach((value) => urlSearchParams.append('responsesTreatment', `${value}`));
+        if (item.response) item.response.forEach((value) => urlSearchParams.append('responsesResponseType', `${value}`));
       });
     }
 
     if (filters.dataAvailable) filters.dataAvailable.forEach((value) => urlSearchParams.append('dataAvailable', `${value}`));
   }
 
-  return searchQuery;
+  const searchString = urlSearchParams.toString();
+  return searchString.length ? `?${searchString}` : "";
 };
 
 export const exportData = (filters?: FilterModel) => {
   return api
     .get("/export" + getApiQuery(filters), { responseType: "blob", timeout: 90000 })
     .then((response) => {
-      fileDownload(
-        response.data,
-        `PTX_Data_Export_${new Date().toLocaleDateString()}.xlsx`
-      );
+      if (response && "data" in response) fileDownload(response.data, `PTX_Data_Export_${new Date().toLocaleDateString()}.xlsx`);
       return true;
     });
 };
