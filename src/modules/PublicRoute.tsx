a14688140e5b0097cf63ede9fb50101169f8a710
AlekseyG <agolovin@mentalstack.com>
2021-11-03 10:38:47 +0300

    Fix public + private routes FC

--- src/modules/PublicRoute.tsx
+++ src/modules/PublicRoute.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { routes} from "../routes";
-import { useAppContext } from '../context';
+import { routes } from "../routes";
+import storage from "../services/storage.service";
 
-const PublicRoute = ({ component, ...rest }: any) => {
-  const { user } = useAppContext();
-  const isAuthorized = user && !!user.isAuthorized;
-
-  const routeComponent = (props: any) => (
-    !isAuthorized
-      ? React.createElement(component, props)
-      : <Redirect to={{pathname: routes.dashboard}}/>
+const PublicRoute = ({ component: Component, ...rest }: any) => {
+  return (
+    <Route {...rest} render={(props: any): React.ReactElement => {
+      const isAuth = storage.checkBool("is_authorized");
+      const accessExp = storage.get("access_token_expires");
+      const isAuthorized = !isAuth || !accessExp || accessExp <= Date.now();
+      if (isAuthorized) return <Redirect to={{ pathname: routes.dashboard }} />
+      return <Component {...props} />;
+    }} />
   );
-
-  return <Route {...rest} render={routeComponent}/>;
 };
 
 export default PublicRoute;
